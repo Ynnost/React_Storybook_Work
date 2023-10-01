a83475d52eeed93fd42ae0e5af6887a5f0e1e879
Ynnost <ynnost94@gmail.com>
2023-10-01 21:28:06 +0300

    12 - uncontrolled input

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {ChangeEvent, useRef, useState} from "react";
 
 export default {
   title: 'input',
@@ -8,14 +8,34 @@ export default {
 
 
 export const UncontrolledInput  = () => <input/>
-
-export const ControlledInputWithFixedValue  = () => <input value={'IT-incubator.by'}/>
-
+//----------------------------------------------------
 export const TrackValueUncontrolledInput  = () =>{
   const [value, setValue]=useState("")
-  return <> <input onChange={(event)=>{
+
+  const onChange = (event:ChangeEvent<HTMLInputElement>)=>{
     const actualValue = event.currentTarget.value
-    setValue(actualValue)
-  }
-  }/> - {value} </>
+    setValue(actualValue)}
+
+  return <> <input onChange={onChange}/> - {value} </>
 }
+//----------------------------------------------------
+export const GetValueUncontrolledInputBuButtonPressByRef  = () =>{
+  const [value, setValue]=useState("")
+  const inputRef = useRef<HTMLInputElement>(null)
+  const save = ()=>{
+    const el = inputRef.current as HTMLInputElement
+    setValue(el.value)}
+
+  return <> <input ref={inputRef} id={'inputId'}/> <button onClick={save}> Save </button> - actual value: {value} </>
+}
+//----------------------------------------------------
+
+export const GetValueUncontrolledInputBuButtonPressById  = () =>{
+  const [value, setValue]=useState("")
+  const save = ()=>{
+    const el = document.getElementById('inputId') as HTMLInputElement
+    setValue(el.value)}
+  return <> <input id={'inputId'}/> <button onClick={save}> Save </button> - actual value: {value} </>
+}
+//----------------------------------------------------
+export const ControlledInputWithFixedValue  = () => <input value={'IT-incubator.by'}/>
